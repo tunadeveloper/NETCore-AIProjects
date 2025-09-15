15676e3b73ada513b97ed451f3ab88e85bc122f8
tunadeveloper <tunabusiness25@gmail.com>
2025-09-15 19:50:31 +0300

    Updated

---
+++ 04-AIProject.OpenAIChat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _04_AIProject.OpenAIChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
