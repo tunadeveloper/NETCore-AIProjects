a750312a77852f5e94a7abc7b940346b9e6dfe4f
tunadeveloper <tunabusiness25@gmail.com>
2025-09-18 22:29:57 +0300

    Updated

---
+++ 07-AIProject.TesseractOcr/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _07_AIProject.TesseractOcr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
