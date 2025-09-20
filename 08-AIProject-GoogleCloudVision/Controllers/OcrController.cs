ceb5b9217ba548e69ad172a9e1659270391b0c14
tunadeveloper <tunabusiness25@gmail.com>
2025-09-20 14:05:31 +0300

    Updated

---
+++ 08-AIProject-GoogleCloudVision/Controllers/OcrController.cs
@@ -0,0 +1,44 @@
+﻿using Google.Cloud.Vision.V1;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _08_AIProject_GoogleCloudVision.Controllers
+{
+    public class OcrController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormFile imageFile)
+        {
+            string credentialPath = "C:\\Users\\tunah\\Desktop\\mystical-banner-472710-k7-0de4e124ec09.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
+
+            try
+            {
+                var client = ImageAnnotatorClient.Create();
+
+                using (var stream = imageFile.OpenReadStream())
+                {
+                    var image = Image.FromStream(stream);
+                    var response = client.DetectText(image);
+                    foreach (var item in response)
+                    {
+                        if (!string.IsNullOrEmpty(item.Description))
+                        {
+                            ViewBag.description = item.Description;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.error = "Hata: " + ex.Message;
+            }
+            return View();
+        }
+    }
+}
--- 08-AIProject.GoogleCloudVision/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _08_AIProject.GoogleCloudVision.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
