8d09932b7be74de3e3e7a1985ace9947bcb87da6
tunadeveloper <tunabusiness25@gmail.com>
2025-09-20 14:08:01 +0300

    Updated

---
+++ 09-AIProject-OpenAITranslate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _09_AIProject_OpenAITranslate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
