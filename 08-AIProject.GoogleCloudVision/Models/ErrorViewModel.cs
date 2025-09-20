29c1a3784fbd210f3d2bbd9e9bc8bbf6dd58f600
tunadeveloper <tunabusiness25@gmail.com>
2025-09-20 13:04:04 +0300

    Updated

---
+++ 08-AIProject.GoogleCloudVision/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _08_AIProject.GoogleCloudVision.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
