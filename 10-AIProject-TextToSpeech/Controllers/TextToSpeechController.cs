2335bb600152964d48f4b7b49208b017b0953a89
tunadeveloper <tunabusiness25@gmail.com>
2025-09-22 10:58:46 +0300

    Updated

---
+++ 10-AIProject-TextToSpeech/Controllers/TextToSpeechController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Speech.Synthesis;
+namespace _10_AIProject_TextToSpeech.Controllers
+{
+    public class TextToSpeechController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string input)
+        {
+            SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+
+            speechSynthesizer.Volume = 100;
+            speechSynthesizer.Rate = 0;
+            
+            if(!string.IsNullOrEmpty(input))
+            {
+                speechSynthesizer.Speak(input);
+            }
+            return View();
+        }
+    }
+}
