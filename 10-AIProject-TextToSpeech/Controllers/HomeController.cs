700a89a6e20ada4a81f766ee4a874580364c39e4
tunadeveloper <tunabusiness25@gmail.com>
2025-09-22 10:48:34 +0300

    Updated

---
+++ 10-AIProject-TextToSpeech/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using _10_AIProject_TextToSpeech.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _10_AIProject_TextToSpeech.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 10-AIProject-TextToSpeech/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _10_AIProject_TextToSpeech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
