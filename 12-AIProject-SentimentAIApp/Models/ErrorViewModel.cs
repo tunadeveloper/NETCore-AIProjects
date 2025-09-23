aacba489da6862f86eefeef546d950dedfb5c026
tunadeveloper <tunabusiness25@gmail.com>
2025-09-23 10:34:15 +0300

    Updated

---
+++ 12-AIProject-SentimentAIApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _12_AIProject_SentimentAIApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
