ca89185bc6aa3183f7e5fd68d69f92a0c552c696
tunadeveloper <tunabusiness25@gmail.com>
2025-09-23 11:43:23 +0300

    Updated

---
+++ 14-AIProject-ArticleSummarizeAI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _14_AIProject_ArticleSummarizeAI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
